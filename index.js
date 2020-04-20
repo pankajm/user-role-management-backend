90d5319923555456efd6d2a53c696f950973b946
pankaj marghade <p.g.marghade@gmail.com>
2020-04-20 12:48:15 +0530

    added apis for roles

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ let users = [
   {id:shortid.generate(), name:'John Williamson', email:'john10@gmail.com', role:'Admin'},
   {id:shortid.generate(), name:'Will Smith', email:'willS@gmail.com', role:'Supervisor'},
   {id:shortid.generate(), name:'Donald wislley', email:'donwislley@yahoo.com', role:'Client'}
-]
+];
+
+let roles = [
+  {id:shortid.generate(), name: 'Admin'}, 
+  {id:shortid.generate(), name: 'Supervisor'}, 
+  {id:shortid.generate(), name: 'Client'}
+];
 
 app.get('/api/users', (req, res, next) => {
   return res.status(200).send(users);
@@ -50,7 +56,34 @@ app.delete('/api/users/:id', (req, res, next) => {
   let indexToDelete = users.indexOf(users.find((user) => user.id == req.params.id));
   users.splice(indexToDelete, 1);
   console.log(users);
-  return res.status(200).send('User Deleted Successfully');
+  return res.status(200).send('User Deleted Successfully.');
 })
 
+// Roles APIs
+
+app.get('/api/roles', (req, res, next) => {
+  return res.status(200).send(roles);
+})
+
+app.post('/api/roles', (req, res, next) => {
+  let role = req.body;
+  role.id = shortid.generate();
+  roles.push(role);
+  console.log(roles);
+  return res.status(200).send(role);
+})
+
+app.delete('/api/roles/:id', (req, res, next) => {
+  let indexToDelete = roles.indexOf(roles.find((role) => role.id == req.params.id));
+  roles.splice(indexToDelete, 1);
+  console.log(roles);
+  return res.status(200).send('Role Deleted Successfully.');
+})
+
+app.put('/api/roles/:id', (req, res, next) => {
+  let role = roles.find((role) => role.id == req.params.id);
+  role.name = req.body.name;
+  console.log(roles);
+  return res.status(200).send('Role updated.');
+})
 
